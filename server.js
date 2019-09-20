11dec6d1b9d6a44f3654ad2e067694ab7ebd6878
Jeremy Yandell <jereyand@gmail.com>
2019-09-20 10:10:41 -0700

    add test routes

--- server.js
+++ server.js
@@ -7,6 +7,13 @@ require('./db/db');
 app.get('/', (req,res) =>  res.send('Hello, Jeremy'));
 
 
+
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/auth', require('./routes/api/auth'));
+app.use('/api/profile', require('./routes/api/profile'));
+app.use('/api/posts', require('./routes/api/posts'));
+
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server listening on port ${PORT}`));

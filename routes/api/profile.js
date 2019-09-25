e4a25ae8b70a49e6a8a9143a14180469bb703647
Jeremy Yandell <jereyand@gmail.com>
2019-09-25 14:45:26 -0700

    add get all profile route

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -102,5 +102,21 @@ router.post(
     }
 );
 
+// @route   GET api/profile
+// @desc    Get all profiles
+// @access  Public
+
+router.get('/', async (req,res) =>  {
+    try {
+        const profiles = await Profile.find().populate('user', ['name']);
+        res.json(profiles);
+    } catch(err) {
+        console.error(err.message);
+        res.status(500).send('Server Error');
+    }
+});
+
+
+
 
 module.exports = router;
